09065734ae478cf2000a2a0cef322890adc790d5
JWLee77 <150890275+JWLee77@users.noreply.github.com>
2023-11-22 16:44:02 +0900

    Update convert.py

--- convert.py
+++ convert.py
@@ -14,18 +14,18 @@ def convert():
     parser.add_argument('-p', '--pixel_size', type=int, default=16, help='individual pixel size')
     parser.add_argument('-e', '--edge_thresh', type=int, default=100, help='lower edge threshold means more black line in edge region')
     args = parser.parse_args()
-
-    img_input = Image.open(args.input)
-    img_pt_input = img_common_util.convert_image_to_tensor(img_input)
+    #값을 입력받고
+    img_input = Image.open(args.input)#PIL라이브러리를 사용하여 이미지 파일을 열고 로드
+    img_pt_input = img_common_util.convert_image_to_tensor(img_input)#부모 클래스를 통해 이미지를 Pytorch Tensor배열로 변환
 
     model = Photo2PixelModel()
     model.eval()
-    with torch.no_grad():
-        img_pt_output = model(
-            img_pt_input,
-            param_kernel_size=args.kernel_size,
-            param_pixel_size=args.pixel_size,
-            param_edge_thresh=args.edge_thresh
+    with torch.no_grad():#기울기를 계산하지 않아 연산 속도 향상
+        img_pt_output = model(#부모 클래스를 통해 계산
+            img_pt_input,#이미지의 Tensor배열
+            param_kernel_size=args.kernel_size,#입력받은 값
+            param_pixel_size=args.pixel_size,#입력받은 값
+            param_edge_thresh=args.edge_thresh#입력받은 값
         )
     img_output = img_common_util.convert_tensor_to_image(img_pt_output)
     img_output.save(args.output)
